053c239582f2bb809a16370d5b2bbdb0de57c853
DjxDeaf <eliranbz@gmail.com>
2014-07-05 03:17:38 +0300

    Some comments added and path fixing

--- script.sh
+++ script.sh
@@ -20,14 +20,14 @@ prefixes=(a b c)
 num_prefixes=${#prefixes[*]}
 
 #Run for each folder
-for dir in /tmp/test/results/*/;
+for dir in *;
 do
         cd $dir
-	#Run for each file in each folder
+		#Run for each file in each folder
         for i in {1..100}
         do
 				
-                        #Creating Random Filenames
+            #Creating Random Filenames
 			FILENAME=`mktemp -u XXXXXXX."${prefixes[$((RANDOM%num_prefixes))]}"`
 			#Creating Random File Size (50Kb to 200Kb)
 			FILESIZE=`expr $(((RANDOM%154+51)*1000)) + $((RANDOM%999+1))`
@@ -35,15 +35,25 @@ do
                         cat /dev/urandom |tr -dc A-Z-a-z-0-9-" " | head -c${1:-$FILESIZE} > $FILENAME
 			echo $FILENAME
 
-			#Removing Spaces
+			#Removing Spaces From file content using a tmp file manipulation
 			sed -r 's/\s+//g' $FILENAME > tmpfile
 			cat tmpfile > $FILENAME
+			
 			#Removing tmpfile for next use
 			rm -f tmpfile
 			
 			#Create MD5 File
 			md5sum $FILENAME > $FILENAME.md5
         done
-	#Exit folder
+		#Exit folder
         cd ..
 done
+
+mailer {
+sendmail eliranbz@gmail.com <<EOF
+subject: test
+from: Script@scriptland.com
+
+Something went wrong!
+EOF
+}
